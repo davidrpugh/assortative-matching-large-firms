bba75de56cf7eb10d23364f0b0529723fed03fc4
David R. Pugh <drobert.pugh@gmail.com>
2014-09-30 12:43:14 -0400

    Added a distribution property to the input class.

--- inputs.py
+++ inputs.py
@@ -22,7 +22,32 @@ class Input(object):
             Dictionary of distribution parameters.
 
         """
-        self.distribution = distribution
         self.lower = bounds[0]
         self.upper = bounds[1]
+        self.distribution = distribution
         self.params = params
+
+    @property
+    def distribution(self):
+        """
+        Probability distribution function (CDF).
+
+        :getter: Return the current distribution function.
+        :setter: Set a new distribution function.
+        :type: sym.Basic
+
+        """
+        return self._distribution
+
+    @distribution.setter
+    def distribution(self, value):
+        """Set a new probability distribution function."""
+        self._distribution = self._validate_distribution(value)
+
+    @staticmethod
+    def _validate_distribution(cdf):
+        """Validates the probability distribution function."""
+        if not isinstance(cdf, sym.Basic):
+            raise AttributeError
+        else:
+            return cdf
