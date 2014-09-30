fc04806b9aa86d9fa84f1448273bfd3f6a657336
David R. Pugh <drobert.pugh@gmail.com>
2014-09-30 16:14:58 -0400

    Added F and params properties.

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+import sympy as sym
+
+
 class Model(object):
     """Class representing a matching model with two-sided heterogeneity."""
 
@@ -15,3 +18,52 @@ class Model(object):
         """
         self.F = production_function
         self.params = params
+
+    @property
+    def F(self):
+        """
+        Symbolic expression describing the available production technology.
+
+        :getter: Return the current production function.
+        :setter: Set a new production function.
+        :type: sym.Basic
+
+        """
+        return self._F
+
+    @F.setter
+    def F(self, value):
+        """Set a new production function."""
+        self._F = self._validate_production_function(value)
+
+    @property
+    def params(self):
+        """
+        Dictionary of model parameters.
+
+        :getter: Return the current parameter dictionary.
+        :setter: Set a new parameter dictionary.
+        :type: dict
+
+        """
+        return self._params
+
+    @params.setter
+    def params(self, value):
+        """Set a new parameter dictionary."""
+        self._params = self._validate_params(value)
+
+    @staticmethod
+    def _validate_params(params):
+        """Validates the dictionary of model parameters."""
+        if not isinstance(params, dict):
+            mesg = "Attribute 'params' must have type dict, not {}."
+            raise AttributeError(mesg.format(params.__class__))
+
+    @staticmethod
+    def _validate_production_function(F):
+        """Validates the production function attribute."""
+        if not isinstance(F, sym.Basic):
+            mesg = "Attribute 'F' must have type sympy.Basic, not {}."
+            raise AttributeError(mesg.format(F.__class__))
+    
