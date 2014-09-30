79e51eb8c7ef857dce22ad2b8077da6295b9dc1d
David R. Pugh <drobert.pugh@gmail.com>
2014-09-30 12:59:44 -0400

    Changed name of distribution property to cdf.

--- inputs.py
+++ inputs.py
@@ -6,7 +6,7 @@ import sympy as sym
 class Input(object):
     """Class representing a heterogenous production input."""
 
-    def __init__(self, bounds, distribution, params):
+    def __init__(self, bounds, cdf, params):
         """
         Create an instance of the Input class.
 
@@ -15,20 +15,20 @@ class Input(object):
         bounds : list
             List containing the lower and uppoer bounds on the support of the
             probability distribution.
-        distribution : sym.Basic
+        cdf : sym.Basic
             Symbolic expression defining a valid probability distribution
-            function.
+            function (CDF).
         params : dict
             Dictionary of distribution parameters.
 
         """
         self.lower = bounds[0]
         self.upper = bounds[1]
-        self.distribution = distribution
+        self.cdf = cdf
         self.params = params
 
     @property
-    def distribution(self):
+    def cdf(self):
         """
         Probability distribution function (CDF).
 
@@ -37,12 +37,12 @@ class Input(object):
         :type: sym.Basic
 
         """
-        return self._distribution
+        return self._cdf
 
-    @distribution.setter
-    def distribution(self, value):
-        """Set a new probability distribution function."""
-        self._distribution = self._validate_distribution(value)
+    @cdf.setter
+    def cdf(self, value):
+        """Set a new probability distribution function (CDF)."""
+        self._cdf = self._validate_cdf(value)
 
     @property
     def lower(self):
@@ -62,6 +62,23 @@ class Input(object):
         self._lower = self._validate_lower_bound(value)
 
     @property
+    def params(self):
+        """
+        Dictionary of distribution parameters.
+
+        :getter: Return the current parameter dictionary.
+        :setter: Set a new parameter dictionary.
+        :type: dict
+
+        """
+        return self._params
+
+    @params.setter
+    def params(self, value):
+        """Set a new parameter dictionary."""
+        self._params = self._validate_params(value)
+
+    @property
     def upper(self):
         """
         Upper bound on support of the probability distribution function (CDF).
@@ -79,8 +96,8 @@ class Input(object):
         self._upper = self._validate_upper_bound(value)
 
     @staticmethod
-    def _validate_distribution(cdf):
-        """Validates the probability distribution function."""
+    def _validate_cdf(cdf):
+        """Validates the probability distribution function (CDF)."""
         if not isinstance(cdf, sym.Basic):
             raise AttributeError
         else:
@@ -92,6 +109,16 @@ class Input(object):
             raise AttributeError
         elif value > self.upper:
             raise AttributeError
+        else:
+            return value
+
+    @staticmethod
+    def _validate_params(value):
+        """Validate the dictionary of parameters."""
+        if not isinstance(value, dict):
+            raise AttributeError
+        else:
+            return value
 
     def _validate_upper_bound(self, value):
         """Validate the upper bound on the suppport of the CDF."""
@@ -99,3 +126,5 @@ class Input(object):
             raise AttributeError
         elif value < self.lower:
             raise AttributeError
+        else:
+            return value
